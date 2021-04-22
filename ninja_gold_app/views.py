914acbc1f9289bc1231189060f71bc73c52a0f3c
elena-zelenskaya <elena.a.zelenskaya@gmail.com>
2021-04-21 22:25:21 -0700

    added seconds in activity

--- ninja_gold_app/views.py
+++ ninja_gold_app/views.py
@@ -8,7 +8,6 @@ def index(request):
     if 'activity' not in request.session and 'gold_sum' not in request.session:
         request.session['activity'] = []
         request.session['gold_sum'] = 0
-    # print(request.session["color"])
     context = {
 		"gold_amount": request.session["gold_sum"],
 		"activity_list": request.session["activity"],
@@ -18,7 +17,7 @@ def index(request):
 def process_money(request):
     if request.method == "POST":
         dateTimeObj = datetime.now()
-        timestampStr = dateTimeObj.strftime("%Y/%m/%d %I:%M %p")
+        timestampStr = dateTimeObj.strftime("%Y/%m/%d %I:%M:%S %p")
         hidden_input_value = request.POST["gold"]
         if hidden_input_value == '1':
             rand_gold = round(10 + random.random() * 10)
@@ -43,8 +42,5 @@ def process_money(request):
         return redirect("/")
 
 def reset(request):
-    # request.session["gold_sum"] = 0
-    # request.session["activity"] = []
-    # request.session["color"] = ""
     request.session.flush()
     return redirect("/")
